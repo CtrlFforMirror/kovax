327fd197cbb679e404cc0d9006534b0dfc73ae18
Freddie Blackman <freddieblackman@Freddies-MacBook-Pro-2.local>
2016-04-23 19:13:37 +0100

    started building nav

---
+++ app/scripts/main.js
@@ -0,0 +1,7 @@
+$('.mobile-nav-button--open').on('click', function() {
+  $('.mobile-nav-container').fadeIn();
+});
+
+$('.mobile-nav-button--close').on('click', function() {
+  $('.mobile-nav-container').fadeOut();
+});
