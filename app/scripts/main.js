43226f87a05e3897883ce11b3a9c81810cf5a5d3
Freddie Blackman <freddieblackman@Freddies-MacBook-Pro-2.local>
2016-05-02 14:10:03 +0100

    nav close button and social media icons opacity

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,18 @@
-$('.mobile-nav-button--open').on('click', function() {
-  $('.mobile-nav-container').fadeIn();
+var mobileNavContainer = $('.mobile-nav-container');
+var socialIcon = $('.social-icon');
+
+$('.mobile-nav-button').on('click', function() {
+  if ($(this).hasClass('mobile-nav-button--open')) {
+    mobileNavContainer.addClass('mobile-nav-container--open');
+  } else {
+    mobileNavContainer.removeClass('mobile-nav-container--open');
+  }
 });
 
-$('.mobile-nav-button--close').on('click', function() {
-  $('.mobile-nav-container').fadeOut();
+socialIcon.mouseenter(function(){
+  $(this).fadeTo(300, 0.7);
+});
+
+socialIcon.mouseleave(function(){
+  $(this).fadeTo(300, 1);
 });
