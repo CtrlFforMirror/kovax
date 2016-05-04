c1f9d82a096a1f5618a21b09809e69eca50ddd32
Freddie Blackman <freddieblackman@Freddies-MacBook-Pro-2.local>
2016-05-04 20:41:46 +0100

    finished contact form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,3 +16,7 @@ socialIcon.mouseenter(function(){
 socialIcon.mouseleave(function(){
   $(this).fadeTo(300, 1);
 });
+
+$('.send-button').on('click', function() {
+  alert('Thank you for getting in touch. We will get back to you as soon as possible.');
+});
