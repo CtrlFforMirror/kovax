41ffb01fc85a757417e055b4c618b9b4e3c68eb7
Freddie Blackman <freddieblackman@campus-10-41-61-126.wireless.leeds.ac.uk>
2016-05-05 14:15:59 +0100

    attempted to correct form validation errors

--- app/scripts/pages/contact.js
+++ app/scripts/pages/contact.js
@@ -1,3 +1,16 @@
 $('.send-button').on('click', function() {
   alert('Thank you for getting in touch. We will get back to you as soon as possible.');
 });
+
+function validate_form ()
+{
+    valid = true;
+
+    if ('.contact-form'.value === "")
+    {
+        alert ( "Please fill in the required fields");
+        valid = false;
+    }
+
+    return valid;
+}
