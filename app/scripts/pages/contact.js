c5045abaebd8455eedf364147b6a2f2f6484bf10
Bertie Blackman <bertie.blackman@hl0042.creativelynx.ad>
2016-05-05 16:54:18 +0100

    Fixed form validation

--- app/scripts/pages/contact.js
+++ app/scripts/pages/contact.js
@@ -1,16 +1,12 @@
-$('.send-button').on('click', function() {
-  alert('Thank you for getting in touch. We will get back to you as soon as possible.');
+$('.send-button').on('click', function (e) {
+	var formIsValid = document.getElementById('contactForm').checkValidity();
+
+	e.preventDefault();
+
+	if (formIsValid) {
+		alert('Thank you for getting in touch! We will contact you as soon as possible');
+	} else {
+		return false
+	}
+
 });
-
-function validate_form ()
-{
-    valid = true;
-
-    if ('.contact-form'.value === "")
-    {
-        alert ( "Please fill in the required fields");
-        valid = false;
-    }
-
-    return valid;
-}
